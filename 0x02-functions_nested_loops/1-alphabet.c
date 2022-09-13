ec3074659b548f2e2d6e2a43276b8f5fcec05b5b
RennersClouds <rennerclouds@gmail.com>
2022-09-13 17:45:42 +0000

    Functions and while loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - print_alphabet
+* Return: return 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory is abuut nested loops on ALX SE low level programming
